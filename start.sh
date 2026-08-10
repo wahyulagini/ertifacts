b9501669b615cd6e1574248cdd39f8dc6aa2e091
wahyulagini <dawwahyu@gmail.com>
2026-08-10 13:18:25 +0700

    fix: add storage:link on startup for Railway deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-﻿#!/bin/sh
+#!/bin/sh
 set -e
 echo '>>> Running: php artisan migrate --force'
 php artisan migrate --force
+echo '>>> Creating storage symlink...'
+php artisan storage:link --force
 echo '>>> Migration done. Starting server...'
 exec php artisan serve --host 0.0.0.0 --port ${PORT:-8080}
